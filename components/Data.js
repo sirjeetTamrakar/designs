f1ad6378dddd9af50925f768657de2bda24c4abe
Srijeet-Tamrakar <tsirjeet220@gmail.com>
2021-08-08 20:32:23 +0545

    sadsadsa

---
+++ components/Data.js
@@ -0,0 +1,24 @@
+export const spidermans = [
+					{
+						id: 1,
+						imageURL: "/2.png",
+						title: "Black Spiderman",
+					},
+					{
+						id: 2,
+						imageURL: "/4.png",
+						title: " Spiderman",
+					},
+
+					{
+						id: 3,
+						imageURL: "/5.png",
+						title: "Black",
+					},
+
+					{
+						id: 4,
+						imageURL: "/6.png",
+						title: "Spiderman",
+					},
+				];
--- components/Hero1.js
+++ components/Hero1.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from "../styles/Hero1.module.css";
 import Image from "next/image";
+import { spidermans } from './Data';
 
 const Hero1 = () => {
+	const [index, setIndex] = useState(0)
+	const {id, title, imageURL } = spidermans[index]
+	console.log(index)
+
+	const check = (number) =>
+	{
+		if (number > spidermans.length - 1)
+		{
+			return 0
+		}
+		if (number < 0)
+		{
+			return spidermans.length-1
+		}
+		return number
+	}
+	const next = () =>
+	{
+		setIndex((index) =>
+		{
+			let newIndex = index + 1
+			return check(newIndex)
+		})
+	}
+
+	const prev = () => {
+		setIndex(index => {
+			let newIndex = index - 1;
+			return check(newIndex);
+		});
+	};
     return (
 			<div className={styles.main}>
 				<div className={styles.left}>
@@ -12,36 +44,42 @@ const Hero1 = () => {
 				<div className={styles.image}>
 					<Image
 						className={styles.img}
-						src='/2.png'
-						height={650}
-					width={850}
+						src={imageURL}
+						height={500}
+					width={500}
+					objectFit='cover'
 						alt='asd'
 					/>
 				</div>
-				<div className={styles.left}></div>
+				<div className={styles.right}>
+					<div className={styles.buttons}>
+						<span className='material-icons' onClick={next}>arrow_forward_ios</span>
+						<span className='material-icons' onClick={prev}>arrow_back_ios</span>
+					</div>
+				</div>
 				<div className={styles.bottom}>
 					<div />
-				<div className={styles.texts}>
+					<div className={styles.texts}>
 						<div className={styles.text}>
 							<h3>Photography</h3>
 							<small>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry.
-						</small>
+								Lorem Ipsum is simply dummy text of the printing and typesetting
+								industry.
+							</small>
 						</div>
 						<div className={styles.text}>
 							<h3>Photography</h3>
 							<small>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry.
-						</small>
+								Lorem Ipsum is simply dummy text of the printing and typesetting
+								industry.
+							</small>
 						</div>
 						<div className={styles.text}>
 							<h3>Photography</h3>
 							<small>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry.
-						</small>
+								Lorem Ipsum is simply dummy text of the printing and typesetting
+								industry.
+							</small>
 						</div>
 					</div>
 					<div />
